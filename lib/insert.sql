06cf152957dcf8c8d5e07f16beeb5542b10f29a4
seanpgallivan <seanpgallivan@hotmail.com>
2019-11-28 09:41:03 -0800

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+    ("Mama", 10, "female", "brown", "too much", true),
+    ("Papa", 10, "male", "brown", "not enough", true),
+    ("Baby", 10, "female", "brown", "just right", true),
+    ("Yogi", 10, "male", "yellow", "hungry", true),
+    ("Berenstein", 10, "female", "brown", "beary", true),
+    ("NY", 10, "male", "blue", "losery", false),
+    (NULL, 10, "female", "brown", "beary", true),
+    ("Cal", 10, "male", "brown", "winnery", true);
